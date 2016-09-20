331dc42af917b1349dab7cc906f326765df6ef06
Herman Banken <hermanbanken@gmail.com>
2016-09-20 13:19:10 +0200

    cleanup fmt downloads

--- precommit.bash
+++ precommit.bash
@@ -21,9 +21,15 @@ if [ ! -f texpty ]; then
     if [ ! -f texpretty-0.02.jar ]; then
       echo "Downloading TeX prettyprinter"
       wget http://ftp.math.utah.edu/pub/texpretty/texpretty-0.02.jar
-      jar xf texpretty-0.02.jar
+      jar xf texpretty-0.02.jar && rm texpretty-0.02.jar
     fi
-    (cd texpretty-0.02 && chmod +x configure && ./configure && make && mv texpty ..)
+    (\
+     cd texpretty-0.02 && \
+     chmod +x configure && \
+     ./configure && make && \
+     mv texpty ..\
+    ) && \
+    rm -rf texpretty-0.02
 fi
 
 function texfmt() {
