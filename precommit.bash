391b8494ae89376df57aba6cba9b2b3187ac04d5
Herman Banken <hermanbanken@gmail.com>
2016-09-20 12:15:54 +0200

    Add precommit fmt's like gofmt

---
+++ precommit.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+####
+# Google Style Guide formatting for Java
+# https://github.com/google/google-java-format
+####
+
+if [ ! -f google-java-format-1.0-all-deps.jar ]; then
+  echo "Downloading google-java-format"
+  wget https://github.com/google/google-java-format/releases/download/google-java-format-1.0/google-java-format-1.0-all-deps.jar
+fi
+
+javafmt="java -jar google-java-format-1.0-all-deps.jar --replace"
+find . -name "*.java" -exec $javafmt {} +
+
+####
+# LaTeX formatter texpretty: TeX prettyprinter
+####
+
+if [ ! -f texpty ]; then
+    if [ ! -f texpretty-0.02.jar ]; then
+      echo "Downloading TeX prettyprinter"
+      wget http://ftp.math.utah.edu/pub/texpretty/texpretty-0.02.jar
+      jar xf texpretty-0.02.jar
+    fi
+    (cd texpretty-0.02 && chmod +x configure && ./configure && make && mv texpty ..)
+fi
+
+function texfmt() {
+  ./texpty --no-comment-banner < $1 > $1.tmp && mv $1.tmp $1
+}
+export -f texfmt
+find . -name "*.tex" -exec bash -c 'texfmt "$@"' bash {} \;
+# latexindent.pl -w **/*.tex
