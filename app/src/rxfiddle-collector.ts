171eb295bbf0bfb6c4bea350c57e809d6f8fc0b3
Herman Banken <hermanbanken@gmail.com>
2016-10-13 15:39:55 +0200

    Working rudimentary logging

--- app/src/rxfiddle-collector.ts
+++ app/src/rxfiddle-collector.ts
@@ -3,10 +3,13 @@ import "./utils";
 import * as Rx from "rx";
 
 let defaultSubjects = {
-    "Rx.Observable": Rx.Observable,
-    "Rx.Observable.prototype": Rx.Observable.prototype,
-    "AnonymousObserver": Rx.AnonymousObserver.prototype,
+    "Observable": Rx.Observable,
+    "Observable.prototype": Rx.Observable.prototype,
+    "AbstractObserver.prototype": Rx.internals.AbstractObserver.prototype,
+    "AnonymousObserver.prototype": Rx.AnonymousObserver.prototype,
   };
+
+let i = 0;
 
 export default class Instrumentation {
   public logger: Visualizer;
@@ -15,7 +18,37 @@ export default class Instrumentation {
   constructor(subjects: { [name: string]: any; } = defaultSubjects, logger: Visualizer = new Visualizer()) {
     this.subjects = subjects;
     this.logger = logger;
-    Object.keys(subjects).slice(0, 2).forEach((s: string) => subjects[s][IGNORE] = true);
+    Object.keys(subjects).slice(0, 1).forEach((s: string) => subjects[s][IGNORE] = true);
+  }
+
+  public instrument(fn, extras) {
+    let calls = this.calls;
+    let logger = this.logger;
+
+    let instrumented = function instrumented() {
+      let call: ICallRecord = {
+        arguments: [].slice.call(arguments, 0),
+        method: extras.methodName,
+        returned: null,
+        stack: new Error().stack,
+        subject: this,
+        subjectName: extras.subjectName,
+        time: performance.now(),
+        id: i++,
+      };
+      calls.push(call);
+      let returned = fn.apply(this, arguments);
+      call.returned = returned;
+      logger.log(call);
+      return returned;
+    };
+
+    instrumented.__originalFunction = fn;
+    return instrumented;
+  }
+
+  public deinstrument(fn) {
+    return fn.__originalFunction || fn;
   }
 
   public setup(): void {
@@ -25,28 +58,15 @@ export default class Instrumentation {
         .map(key => ({ key, name: name as string, subject }))
       )
       .reduce((prev, next) => prev.concat(next), []);
+
     let methods = properties
       .filter(({ key, subject }) => typeof subject[key] === "function");
-    let instrumentation = this;
-    methods.forEach(({ key: methodName, name, subject }) => {
-        let originalMethod = subject[methodName];
-        function instrumented() {
-          let call: ICallRecord = {
-            arguments: [].slice.call(arguments, 0),
-            method: methodName,
-            returned: null,
-            stack: new Error().stack,
-            subject: this,
-            subjectName: name,
-            time: performance.now(),
-          };
-          instrumentation.calls.push(call);
-          let returned = originalMethod.apply(this, arguments);
-          call.returned = returned;
-          instrumentation.logger.log(call);
-          return returned;
-        }
-        subject[methodName] = instrumented;
+
+    methods.forEach(({ key, name, subject }) => {
+      subject[key] = this.instrument(subject[key], {
+        methodName: key,
+        subjectName: name,
       });
+    });
   }
 }
--- app/src/rxfiddle-visualizer.ts
+++ app/src/rxfiddle-visualizer.ts
@@ -1,6 +1,7 @@
 import "./utils";
 import * as dagre from "dagre";
 import * as snabbdom from "snabbdom";
+import * as ErrorStackParser from "error-stack-parser";
 
 const h = require("snabbdom/h");
 const moduleAttrs = require("snabbdom/modules/attributes");
@@ -20,34 +21,51 @@ export interface ICallRecord {
   returned: any | null;
 }
 
+const inst_method = "instrumented";
+const inst_file = "rxfiddle-collector.js";
+
+function endsWith(self: string, suffix: string): boolean {
+  return self.indexOf(suffix, self.length - suffix.length) !== -1;
+};
+
 export class Visualizer {
+
+  private static isNotRxFiddle(frame: any): boolean {
+    return frame.functionName !== inst_method || !endsWith(frame.fileName, inst_file);
+  }
+
   private g = new dagre.graphlib.Graph();
   private unrendered: number = 0;
   private svg: HTMLElement;
-
   constructor() {
     this.g.setGraph({});
     this.g.setDefaultEdgeLabel(() => ({}));
   }
+
   public log(record: ICallRecord) {
+    // if (this.unrendered < 20) {
+    //   var last = ErrorStackParser.parse(record).slice(1, 2)[0];
+    //   var id = record.subjectName + "." + record.method + last.source;
+    //   console.log(record.id, record, id);
+    // }
+
     this.unrendered += 1;
-    if (record.subject[HASH]) {
-      console.log("Reuse")
-    } else if (typeof record.subject[IGNORE] === "undefined") {
-      this.tag(record.subject);
-    } else {
-      record.subject = {};
-      this.tag(record.subject, record.subjectName);
+    if (!record.subject[HASH]) {
+      if (typeof record.subject[IGNORE] === "undefined") {
+        this.tag(record.subject);
+      } else {
+        record.subject = {};
+        this.tag(record.subject, record.subjectName);
+      }
     }
 
-    let operator = this.operator(record.method);
-    this.g.setEdge(record.subject[HASH], operator);
+    if (typeof record.returned !== "undefined") {
+      let operator = this.operator(record.method);
+      this.g.setEdge(record.subject[HASH], operator);
 
-    if (typeof record.returned === "object") {
       this.tag(record.returned);
+      this.g.setEdge(operator, record.returned[HASH]);
     }
-
-    this.g.setEdge(operator, record.returned[HASH]);
   }
   public run() {
     if (this.unrendered === 0) {
@@ -58,9 +76,7 @@ export class Visualizer {
     this.svg.innerHTML = "";
     let ns = this.g.nodes().map((v: string) => {
       let node = this.g.node(v);
-      let label = node.label;
-      delete node.label;
-      // console.log("Node " + v + ": " + JSON.stringify(this.g.node(v)));
+      if (!node) { return undefined; }
       return h("g", { attrs: { transform: `translate(${node.x},${node.y})` } }, node.render());
     });
 
@@ -69,7 +85,6 @@ export class Visualizer {
       let path = "M " + edge.points.map((p: { x: number, y: number }) => `${p.x} ${p.y}`).join(" L ");
       let attrs = { d: path, fill: "transparent", "stroke-width": "5", stroke: "red" };
       return h("path", { attrs });
-      // console.log("Edge " + e.v + " -> " + e.w + ": " + JSON.stringify(this.g.edge(e)));
     });
 
     let childs = ns.concat(es).reduce((chunks, next, i) => {
@@ -91,7 +106,7 @@ export class Visualizer {
 
     patch(this.svg, updated);
     this.svg = updated;
-    let instance = svgPanZoom("#svg");
+    let instance = svgPanZoom("#svg", { maxZoom: 30 });
   }
   public attach(node: HTMLElement) {
     this.svg = node;
