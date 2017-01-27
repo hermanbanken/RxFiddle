ed565ad04521c3daa1578ca6a63fc45cb4d04353
Herman Banken <hermanbanken@gmail.com>
2017-01-27 15:31:54 +0100

    Improve `ordering` to terminate eagerly

--- app/src/layout/ordering.ts
+++ app/src/layout/ordering.ts
@@ -20,20 +20,43 @@ export function ordering(order: string[][], g: Graph): string[][] {
   let best: string[][]
   let bestCrossings: number = Number.MAX_SAFE_INTEGER
 
-  let update = (next: string[][]) => {
-    let crossings = order_crossings(next, g)
-    if (crossings < bestCrossings) {
-      best = order.map(o => o.slice(0))
-      bestCrossings = crossings
+  let bestIt = -1
+  let sameCount = 0
+  let lastCrossings = Number.MAX_SAFE_INTEGER
+
+  let update = (next: string[][], i: number) => {
+    try {
+      // See if improved: store better results
+      let crossings = order_crossings(next, g)
+      if (crossings < bestCrossings) {
+        best = next.map(o => o.slice(0))
+        bestCrossings = crossings
+        bestIt = i
+      }
+      // Abort if stable
+      if (lastCrossings === crossings || crossings === 0) {
+        sameCount++
+        if (sameCount > 3 || crossings === 0) {
+          return false
+        }
+      }
+      lastCrossings = crossings
+    } catch (e) {
+      console.warn("Error working with", next)
+      throw e
     }
+    return true
   }
 
-  update(order)
+  update(order, 0)
 
   for (let i = 0; i < 40; i++) {
     wmedian(order, g, i % 2 === 0 ? "up" : "down")
     transpose(order, g, "down")
-    update(order)
+    transpose(order, g, "up")
+    if (!update(order, i + 1)) {
+      break
+    }
   }
 
   return best
