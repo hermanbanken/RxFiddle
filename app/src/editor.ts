d67108b9650fbc31ed6d909a420931656d904e2e
Herman Banken <hermanbanken@gmail.com>
2017-03-31 10:45:45 +0200

    Forward editor Error's to main window & display error stack

--- app/src/editor.ts
+++ app/src/editor.ts
@@ -19,6 +19,45 @@ if (hash.indexOf("blob=") >= 0) {
   editor.setValue(localStorage.getItem("code"))
 }
 
+function formatError(e: Error): any {
+  return {
+    message: e.message,
+    name: e.name,
+    original: typeof (e as any).original !== "undefined" ? formatError((e as any).original) : undefined,
+    stack: e.stack.toString(),
+  }
+}
+
+/** 
+ * Have single location for evil eval,
+ * so we can infer it's stackTrace beforehand 
+ * and strip that from the errors coming from it 
+ */
+function scopedEval(code: string) {
+  // tslint:disable-next-line:no-eval
+  return eval(code)
+}
+
+function evalAndRepackageErrors(code: string): { type: "result", result: any } | { type: "error", error: any } {
+  try {
+    return { result: scopedEval(code), type: "result" }
+  } catch (e) {
+    // Infer eval location
+    try {
+      scopedEval("throw new Error('ERROR')")
+    } catch (dummyError) {
+      // clean up error stack trace
+      let result = /\n\s+at scopedEval \((.*)\)/.exec(dummyError.stack)
+      let stack: string = e.stack.toString()
+      let index = stack.lastIndexOf(`at scopedEval (${result[1]})`)
+      stack = stack.substring(0, index)
+      stack = stack.split(`eval at scopedEval (${result[1]}), `).join("")
+      e.stack = stack
+    }
+    return { error: formatError(e), type: "error" }
+  }
+}
+
 let instrumentation: Instrumentation
 function run() {
   if (instrumentation) {
@@ -37,8 +76,15 @@ function run() {
   instrumentation.setup()
   poster.reset()
 
-  // tslint:disable-next-line:no-eval
-  eval(code)
+  // Execute user code
+  let result = evalAndRepackageErrors(code)
+
+  if (result.type === "error") {
+    window.parent.postMessage({
+      error: result.error,
+      type: "error",
+    }, location.origin)
+  }
 }
 
 let button = document.querySelector("#run") as HTMLButtonElement
--- app/src/main.ts
+++ app/src/main.ts
@@ -31,21 +31,30 @@ function formatHash(object: any): string {
   return q
 }
 
-const sameOriginWindowMessages = Rx.Observable.fromEvent(window, "message", (e: any) => {
-  // For Chrome, the origin property is in the event.originalEvent object.
-  let origin = e.origin || (e as any).originalEvent.origin
-  if (origin !== window.location.origin) {
-    return
-  }
-  return e.data
-})
+const sameOriginWindowMessages = Rx.Observable
+  .fromEvent(window, "message", (e: any) => {
+    // For Chrome, the origin property is in the event.originalEvent object.
+    let origin = e.origin || (e as any).originalEvent.origin
+    if (origin !== window.location.origin) {
+      return
+    }
+
+    return e.data
+  })
+  .flatMap(event => {
+    // Emit Editor errors in this stream; todo: move this throw elsewhere
+    if (event && event.type === "error") {
+      return Rx.Observable.create(o => o.onError(event.error))
+    }
+    return Rx.Observable.just(event)
+  })
 
 sameOriginWindowMessages
   .map(_ => _.hash)
   .filter(_ => typeof _ !== "undefined")
   .subscribe(hash => {
     window.location.hash = formatHash(hash)
-  })
+  }, e => { /* */ })
 
 class CodeEditor {
   public dom: Rx.Observable<VNode>
@@ -132,17 +141,27 @@ class LanguageMenu {
             "issue Pull Request"
           )),
         ]),
-      ])), language: Rx.Observable.never<LanguageCombination>()
+      ])), language: Rx.Observable.never<LanguageCombination>(),
     }
   }
 }
 
 function errorHandler(e: Error): Rx.Observable<{ dom: VNode, timeSlider: VNode }> {
   let next = Rx.Observable.create(sub => {
-    console.error(e);
-    setTimeout(() => sub.onError(new Error("Continue")), 1000)
+    setTimeout(() => sub.onError(new Error("Continue")), 5000)
   })
-  return Rx.Observable.just({ dom: h("div", "Krak!"), timeSlider: h("div") }).merge(next)
+  return Rx.Observable.just({
+    dom: h("div", [
+      h("p", `Krak!`),
+      h("p", `Global error handling (anything not Rx onError's ) is not yet done. 
+      Also, please make sure to handle errors in your subscribe calls, otherwise they bubble into global errors!`),
+      h("a.btn", { attrs: { href: "javascript:window.location.reload()" } }, "Reload"),
+      h("textarea",
+        { style: { height: "50vh", overflow: "auto", width: "50vw" } },
+        e.stack || JSON.stringify(e, null, 2)
+      ),
+    ]), timeSlider: h("div"),
+  }).merge(next)
 }
 
 function menu(time: VNode, language: VNode, run?: () => void): VNode {
