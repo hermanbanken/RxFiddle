4de3602270c9bd12f331798c6269146b2a32994e
Herman Banken <hermanbanken@gmail.com>
2017-04-14 11:34:41 +0200

    Update experiment screen render loop

--- app/src/experiment.ts
+++ app/src/experiment.ts
@@ -3,13 +3,14 @@
 import RxRunner from "./collector/runner"
 import overlay from "./experiment/overlay"
 import samples, { Sample } from "./experiment/samples"
-import { Screen, generalScreens, introScreen } from "./experiment/screens"
+import { Screen, TestEvent, TestState, generalScreens, introScreen } from "./experiment/screens"
 import { formatSeconds } from "./experiment/utils"
 import patch from "./patch"
 import CodeEditor from "./ui/codeEditor"
 import { hbox, vbox, vboxo } from "./ui/flex"
 import Resizer from "./ui/resizer"
 import { Query, errorHandler } from "./ui/shared"
+import { UUID } from "./utils"
 import Visualizer from "./visualization"
 import { GrapherAdvanced as Grapher } from "./visualization/grapher"
 import * as Rx from "rx"
@@ -24,13 +25,45 @@ import { VNode } from "snabbdom/vnode"
 // other:
 // track all events
 
-type TestState = {
-  id: string
-  paused: boolean
-  start: Date
-  active: string[]
-  data: { [testId: string]: any }
+let initialTestState: TestState = {
+  id: UUID(),
+  paused: true,
+  active: [],
+  data: {},
 }
+
+if (localStorage) {
+  let lastId = localStorage.getItem("lastSurvey")
+  if (lastId) {
+    initialTestState = JSON.parse(localStorage.getItem(lastId))
+  } else {
+    localStorage.setItem(initialTestState.id, JSON.stringify(initialTestState))
+    localStorage.setItem("lastSurvey", initialTestState.id)
+  }
+}
+
+let testEvents = new Rx.Subject<TestEvent>()
+let testLoop = testEvents
+  .scan<TestState>((state, event) => {
+    if (event.type === "start") {
+      return Object.assign({}, state, { id: event.surveyId, paused: false })
+    }
+    return state
+  }, initialTestState)
+  .startWith(initialTestState)
+  .do(console.log)
+  .map(state => {
+    let screen: Screen
+    if (state.paused) {
+      screen = introScreen
+    } else {
+      screen = generalScreens
+    }
+    let { events, dom } = screen.render(state)
+    let forwardEvents = Rx.Observable.create(o => events.subscribe(testEvents))
+    return dom.merge(forwardEvents)
+  })
+  .switchLatest()
 
 function menu(runner?: RxRunner, editor?: CodeEditor): VNode {
   let clickHandler = () => {
@@ -58,74 +91,77 @@ function formatQuestion(sample: Sample, button: Rx.Observable<VNode>, time: Rx.O
 
 /* Screen containing RxFiddle */
 let testScreen = (scheduler: Rx.IScheduler): Screen => ({
-  dom: Rx.Observable.from(samples).concatMap(sample => Rx.Observable.defer(() => {
-    let collector = DataSource(sample.code)
+  render: () => ({
+    dom: Rx.Observable.from(samples).concatMap(sample => Rx.Observable.defer(() => {
+      let collector = DataSource(sample.code)
 
-    let next = Rx.Observable.create<VNode>(observer => {
-      observer.onNext(h("button", { on: { click: () => observer.onCompleted() } }, "Continue with next question"))
-    }).shareReplay(1)
+      let next = Rx.Observable.create<VNode>(observer => {
+        observer.onNext(h("button", { on: { click: () => observer.onCompleted() } }, "Continue with next question"))
+      }).shareReplay(1)
 
-    let pass = Rx.Observable.create<VNode>(observer => {
-      observer.onNext(h("button", { on: { click: () => confirm("Are you sure you to pass on this question?") && observer.onCompleted() } }, "Pass"))
-    }).shareReplay(1)
+      let pass = Rx.Observable.create<VNode>(observer => {
+        observer.onNext(h("button", { on: { click: () => confirm("Are you sure you to pass on this question?") && observer.onCompleted() } }, "Pass"))
+      }).shareReplay(1)
 
-    let question = Rx.Observable.interval(1000, scheduler).take(sample.timeout)
-      .let(time => formatQuestion(sample, pass, time))
+      let question = Rx.Observable.interval(1000, scheduler).take(sample.timeout)
+        .let(time => formatQuestion(sample, pass, time))
 
-    let active = Rx.Observable
-      .defer(() => {
-        let vis = new Visualizer(new Grapher(collector.data), document.querySelector("app") as HTMLElement)
-        return vis.stream()
-      })
-      .catch(errorHandler)
-      .retry()
-      .combineLatest(
-      question.do(vn => console.log("question")),
-      collector.vnode.do(vn => console.log("vnode")),
-      collector.runner.state.do(vn => console.log("runner.state")),
-      (visualizer, questiondom, input, state) => [
-        h("div#menufold-static.menufold", [
-          h("a.brand.left", { attrs: { href: "#" } }, [
-            h("img", { attrs: { alt: "ReactiveX", src: "RxIconXs.png" } }),
+      let active = Rx.Observable
+        .defer(() => {
+          let vis = new Visualizer(new Grapher(collector.data), document.querySelector("app") as HTMLElement)
+          return vis.stream()
+        })
+        .catch(errorHandler)
+        .retry()
+        .combineLatest(
+        question.do(vn => console.log("question")),
+        collector.vnode.do(vn => console.log("vnode")),
+        collector.runner.state.do(vn => console.log("runner.state")),
+        (visualizer, questiondom, input, state) => [
+          h("div#menufold-static.menufold", [
+            h("a.brand.left", { attrs: { href: "#" } }, [
+              h("img", { attrs: { alt: "ReactiveX", src: "RxIconXs.png" } }),
+            ]),
+            menu(collector.runner, collector.editor),
+            h("div", { style: { flex: "1" } }),
+            h("div.right", [
+              h("button.btn", "JavaScript cheatsheet"),
+              h("button.btn", "RxJS docs"),
+              h("button.btn.helpbutton", "Help"),
+              overlay(),
+            ]),
           ]),
-          menu(collector.runner, collector.editor),
-          h("div", { style: { flex: "1" } }),
-          h("div.right", [
-            h("button.btn", "JavaScript cheatsheet"),
-            h("button.btn", "RxJS docs"),
-            h("button.btn.helpbutton", "Help"),
-            overlay(),
-          ]),
-        ]),
-        questiondom,
-        hbox(Resizer.h(
-          "rxfiddle/editor+rxfiddle/inspector",
-          input,
-          vboxo({ class: "viewer-panel" }, visualizer.dom)
-        )),
-      ])
+          questiondom,
+          hbox(Resizer.h(
+            "rxfiddle/editor+rxfiddle/inspector",
+            input,
+            vboxo({ class: "viewer-panel" }, visualizer.dom)
+          )),
+        ])
 
-    let outOfTime: Rx.Observable<VNode[]> = Rx.Observable.just(sample.timeout)
-      .let(time => formatQuestion(sample, pass, time))
-      .takeUntil(next)
-      .combineLatest(next, (render, nextButton) => [
-        render,
-        h("div.center", h("div", { style: { padding: "3em" } }, [
-          h("h2", "time's up"),
-          nextButton,
-          h("p.gray", { style: { width: "21em", "margin-left": "auto", "margin-right": "auto", color: "gray" } },
-            `Don't worry. After the survey, you may spend all the time you
+      let outOfTime: Rx.Observable<VNode[]> = Rx.Observable.just(sample.timeout)
+        .let(time => formatQuestion(sample, pass, time))
+        .takeUntil(next)
+        .combineLatest(next, (render, nextButton) => [
+          render,
+          h("div.center", h("div", { style: { padding: "3em" } }, [
+            h("h2", "time's up"),
+            nextButton,
+            h("p.gray", { style: { width: "21em", "margin-left": "auto", "margin-right": "auto", color: "gray" } },
+              `Don't worry. After the survey, you may spend all the time you
                want in this tool. Your questions, your code and your answers
                will be available at the end of the survey, if you wish.`),
-        ])),
-      ])
+          ])),
+        ])
 
-    return Rx.Observable.of(active)
-      .merge(Rx.Observable.timer(1000 * sample.timeout, scheduler).map(_ => outOfTime))
-      .switch()
-      .takeUntil(pass.last())
-      .map(nodes => h("div.tool.flexy.flexy-v.rel", nodes))
-  })),
+      return Rx.Observable.of(active)
+        .merge(Rx.Observable.timer(1000 * sample.timeout, scheduler).map(_ => outOfTime))
+        .switch()
+        .takeUntil(pass.last())
+        .map(nodes => h("div.tool.flexy.flexy-v.rel", nodes))
+    })),
+    events: Rx.Observable.never<TestEvent>(),
+  }),
 })
 
 function DataSource(code: string) {
@@ -152,9 +188,14 @@ let screens = [
 ]
 
 let mainVN = document.querySelector("#screens") as VNode | HTMLBodyElement
-Rx.Observable
-  .of(...screens)
-  .concatMap(_ => _.dom)
+// Rx.Observable
+// .of(...screens)
+// .concatMap(_ => _.dom)
+// .subscribe(vnode => {
+//   mainVN = patch(mainVN, h("div#screens", [vnode]))
+// })
+
+testLoop
   .subscribe(vnode => {
     mainVN = patch(mainVN, h("div#screens", [vnode]))
   })
--- app/src/experiment/screens.ts
+++ app/src/experiment/screens.ts
@@ -1,10 +1,60 @@
 // tslint:disable:object-literal-sort-keys
+import { UUID } from "../utils"
 import { handleRangeTouch } from "./range"
 import h from "snabbdom/h"
 import { VNode } from "snabbdom/vnode"
 
+export type TestState = {
+  id: string
+  paused: boolean
+  started?: Date
+  active: string[]
+  data: { [testId: string]: any }
+}
+
+export type TestEvent =
+  { type: "start", surveyId: string } |
+  { type: "answer", surveyId: string, path: string[] }
 export type Screen = {
-  dom: Rx.Observable<VNode>
+  render: (state: TestState) => {
+    dom: Rx.Observable<VNode>
+    events: Rx.Observable<TestEvent>
+  }
+}
+
+class States {
+  public static list() {
+    let ids = JSON.parse(localStorage.getItem("surveys") || "[]")
+    return ids.map((id: string) => States.get(id)) as TestState[]
+  }
+  public static get(id: string) {
+    return (JSON.parse(localStorage.getItem(id) || "null") || [States.create(id)]) as TestState
+  }
+  public static create(id?: string) {
+    return {
+      id: id || UUID(),
+      paused: true,
+      active: [],
+      data: {},
+    } as TestState
+  }
+  public static save(state: TestState) {
+    let list = States.list().map(_ => _.id)
+    if (list.indexOf(state.id) < 0) {
+      list.push(state.id)
+      localStorage.setItem("surveys", JSON.stringify(list))
+    }
+    localStorage.setItem(state.id, JSON.stringify(state))
+    localStorage.setItem("lastSurvey", state.id)
+  }
+}
+
+function mkButton(text: string = "Next") {
+  let subject = new Rx.Subject<boolean>()
+  return {
+    dom: h("a.btn", { attrs: { href: "#top" }, on: { click: () => subject.onNext(true) } }, text),
+    events: subject as Rx.Observable<boolean>,
+  }
 }
 
 function wrapWithNextButton(nodes: VNode[], nextText: string = "Next") {
@@ -25,7 +75,7 @@ function q(name: string, text: string, opts: QuestionTypeOptions) {
   let nodes: VNode[] = []
   if (opts.type === "range" || opts.type === "labels") {
     nodes.push(h("input", {
-      attrs: { type: "range", min: opts.min, max: opts.max },
+      attrs: { type: "range", min: opts.min, max: opts.max, value: (opts.min + opts.max) / 2 },
       on: {
         touchstart: (e: TouchEvent) => handleRangeTouch(e),
         touchmove: (e: TouchEvent) => handleRangeTouch(e),
@@ -48,21 +98,45 @@ function q(name: string, text: string, opts: QuestionTypeOptions) {
 
 /* First screen the user sees */
 export let introScreen: Screen = {
-  dom: wrapWithNextButton([
-    h("h1", "Survey on Reactive Programming"),
-    h("p", `This is a survey to test Reactive Program comprehension and debugability.
-       Please take part, even if you have little experience with Reactive Programming.`),
-    h("ul", [
-      h("li", `What's in it for you: learn a new way of debugging Reactive Programs!`),
-      h("li", `What's in it for me: you help me graduate!`),
-      h("li", `Estimated time required: 60 minutes`),
-      h("li", `Used Reactive Programming implementation: RxJS 4.1`),
-    ]),
-    h("div.mobile-only", [
-      h("p", `This survey works best on Chrome on a laptop/desktop. Please set a reminder to take the survey later.`),
-      h("p", h("a.btn", { attrs: { href: "x-apple-reminder://" } }, "Set a reminder")),
-    ]),
-  ], "Start survey"),
+  render: (state) => {
+    let buttons = [States.create(), ...States.list()].map(survey => {
+      let button = mkButton(survey.started ? "Resume Survey" : "Start Survey")
+      return {
+        survey,
+        dom: h("div", [
+          button.dom,
+          survey.started ? `started at ${survey.started}` : "",
+        ]),
+        events: button.events.map(e => ({
+          type: "start",
+          surveyId: survey.id,
+        }) as TestEvent),
+      }
+    })
+    return {
+      dom: Rx.Observable.just(h("div.flexy", h("div.scrolly.flexy", h("div.width", [
+        h("h1", "Survey on Reactive Programming"),
+        h("p", `This is a survey to test Reactive Program comprehension and debugability.
+                Please take part, even if you have little experience with Reactive Programming.`),
+        h("ul", [
+          h("li", `What's in it for you: learn a new way of debugging Reactive Programs!`),
+          h("li", `What's in it for me: you help me graduate!`),
+          h("li", `Estimated time required: 60 minutes`),
+          h("li", `Used Reactive Programming implementation: RxJS 4.1`),
+        ]),
+        h("div.mobile-only", [
+          h("p", `This survey works best on Chrome on a laptop/desktop. 
+                  Please set a reminder to take the survey later.`),
+          h("p", h("a.btn", { attrs: { href: "x-apple-reminder://" } }, "Set a reminder")),
+        ]),
+        ...buttons.map(_ => h("div", [
+          _.dom,
+          _.survey.started ? `started at ${_.survey.started}` : "",
+        ])),
+      ])))),
+      events: Rx.Observable.merge<TestEvent>(buttons.map(_ => _.events)),
+    }
+  },
 }
 
 /* Second screen the user sees */
@@ -73,7 +147,7 @@ let general1: VNode[] = [
 
   h("h2", "Programming in general"),
   q("years_pr", "Your years of experience with programming in general:", { max: 30, min: 0, type: "range" as "range" }),
-  q("exp_pr", "Access your level of experience in programming in general:", {
+  q("exp_pr", "Assess your level of experience in programming in general:", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
@@ -83,23 +157,23 @@ let general1: VNode[] = [
   }),
 
   h("h2", "Languages"),
-  q("exp_js", "Access your level of experience in JavaScript:", {
+  q("exp_js", "Assess your level of experience in JavaScript:", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
-  q("exp_java", "Access your level of experience in Java:", {
+  q("exp_java", "Assess your level of experience in Java:", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
-  q("exp_scala", "Access your level of experience in Scala:", {
+  q("exp_scala", "Assess your level of experience in Scala:", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
-  q("exp_swift", "Access your level of experience in Swift:", {
+  q("exp_swift", "Assess your level of experience in Swift:", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
-  q("exp_c#", "Access your level of experience in C#:", {
+  q("exp_c#", "Assess your level of experience in C#:", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
@@ -112,7 +186,7 @@ let general2: VNode[] = [
 
   h("h2", "Reactive Programming"),
   q("years_rp", "Your years of experience with Reactive Programming:", { max: 10, min: 0, type: "range" as "range" }),
-  q("exp_rp", "Access your level of experience in Reactive Programming:", {
+  q("exp_rp", "Assess your level of experience in Reactive Programming:", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
@@ -121,12 +195,12 @@ let general2: VNode[] = [
     labels: ["worse", "slightly worse", "equal", "slightly better", "better"],
   }),
 
-  q("exp_rx", "Access your level of experience with ReactiveX (RxJS, RxSwift, Rx.NET, etc.):", {
+  q("exp_rx", "Assess your level of experience with ReactiveX (RxJS, RxSwift, Rx.NET, etc.):", {
     max: 8, min: 0, type: "labels" as "labels",
     labels: ["none", "beginner", "medium", "senior", "expert"],
   }),
 
-  q("exp_reactivestreams", `Access your level of experience with 
+  q("exp_reactivestreams", `Assess your level of experience with 
      Reactive Streams implementations 
      (Akka Streams, Spring Project Reactor, Bacon.js, Most.js, etc.):`,
     {
@@ -136,8 +210,11 @@ let general2: VNode[] = [
 ]
 
 export let generalScreens: Screen = {
-  dom: Rx.Observable.concat(
-    wrapWithNextButton(general1),
-    wrapWithNextButton(general2)
-  ),
+  render: (state) => ({
+    dom: Rx.Observable.concat(
+      wrapWithNextButton(general1),
+      wrapWithNextButton(general2)
+    ),
+    events: Rx.Observable.never<TestEvent>(),
+  }),
 }
