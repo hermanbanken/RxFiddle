188463abde3e5890cfdafbf003d03528b294ace5
Herman Banken <hermanbanken@gmail.com>
2017-04-21 16:33:36 +0200

    Update gh_pages

--- gh_pages.bash
+++ gh_pages.bash
@@ -1,4 +1,13 @@
+#!/bin/bash
+set -o errexit
+
+cd app
+webpack
+yarn run test
+yarn run cp-rx
+yarn run distlibs
 git add dist src/instrumentation/rxjs-4.1.0/rx.all.js -f
+cd ..
 git commit -m "Build"
 git branch -D gh-pages
 git subtree split --prefix app -b gh-pages
