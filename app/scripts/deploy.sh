15c0e04a89c31f5c86274e63a3faad0c9f9e68d6
Herman Banken <hermanbanken@gmail.com>
2017-05-02 17:59:06 +0200

    Fix deploy script

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 # Prepare
 touch building.txt # ensure stash is created
 git stash -u
-DEPLOYHEAD=`git rev-parse --verify HEAD`
-git checkout gh-pages
+git branch -D gh-pages-tmp || true
+DEPLOYHEAD=`git name-rev --name-only HEAD`
+git checkout -b gh-pages-tmp
 git reset $DEPLOYHEAD --hard
 
 # Build
@@ -12,7 +15,13 @@ yarn run build
 git add -f $(find dist -type f -maxdepth 1)
 git add -f dist/instrumentation
 git commit -m 'Build'
+
+# Deploy
+cd ..
+git branch -D gh-pages
+git subtree split --prefix app -b gh-pages
 git push -f origin gh-pages
+cd app
 
 # Reset
 git checkout $DEPLOYHEAD
