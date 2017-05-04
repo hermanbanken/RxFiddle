ce49d905833f9620746a93b24ac1ea0ebc10ed67
Herman Banken <hermanbanken@gmail.com>
2017-05-04 11:16:33 +0200

    Clear the console upon restart

--- app/src/experiment/console-visualizer.ts
+++ app/src/experiment/console-visualizer.ts
@@ -8,23 +8,29 @@ export default class ConsoleVisualizer {
   constructor(dataSource: ConsoleRunner) {
     this.dom = dataSource.dataObs
       .repeat()
-      .map(message => {
-        if (message === "reset") {
-          return h("div.reset", "Restarted at " + new Date())
-        }
-        if (typeof message === "object" && "level" in message && "arguments" in message) {
-          return h(`div.level-${message.level}`, message.arguments)
-        }
-        return h("div", JSON.stringify(message))
-      })
-      .scan((list, next) => list.concat([next]), [])
+      .scan((state, message) => {
+        let kept = state.keep < 0 ? state.list.slice(0) : state.list.slice(-state.keep)
+        let keep = message === "reset" ? 1 : -1
+        return { list: kept.concat([{ date: new Date(), message, json: JSON.stringify(message) }]), keep }
+      }, { keep: -1, list: [] })
+      .map(_ => _.list)
       .startWith([])
       .map(list => h("div.console-messages", {
         hook: {
           postpatch: (old, next) => { scrollToBottom(next.elm as HTMLElement) },
         },
-      }, list))
+      }, list.map(format)))
   }
+}
+
+function format({ message, date, json }: { message: any, date: Date, json: string }, i: number) {
+  if (message === "reset") {
+    return h("div.reset", i === 0 ? "Restarted at " + date : "Stopped at " + date)
+  }
+  if (typeof message === "object" && "level" in message && "arguments" in message) {
+    return h(`div.level-${message.level}`, message.arguments)
+  }
+  return h("div", json)
 }
 
 function scrollToBottom(elm: HTMLElement) {
